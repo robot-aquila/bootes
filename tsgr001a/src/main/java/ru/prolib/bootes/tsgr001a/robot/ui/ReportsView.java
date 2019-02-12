34b0118576f12f8f4371e684fdd93cadc3d8c4ef
aquila <robot-aquila@hotmail.com>
2019-02-12 19:30:51 +0300

    tsgr001a: s3 report integrated

--- tsgr001a/src/main/java/ru/prolib/bootes/tsgr001a/robot/ui/ReportsView.java
+++ tsgr001a/src/main/java/ru/prolib/bootes/tsgr001a/robot/ui/ReportsView.java
@@ -3,10 +3,17 @@ package ru.prolib.bootes.tsgr001a.robot.ui;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.table.TableRowSorter;
 
+import ru.prolib.aquila.core.text.IMessages;
+import ru.prolib.aquila.ui.TableModelController;
 import ru.prolib.bootes.lib.app.AppServiceLocator;
+import ru.prolib.bootes.lib.report.s3rep.ui.S3ReportTableModel;
 import ru.prolib.bootes.lib.report.summarep.ui.SummaryReportView;
+import ru.prolib.bootes.lib.service.UIService;
 import ru.prolib.bootes.tsgr001a.robot.RoboServiceLocator;
 import ru.prolib.bootes.tsgr001a.robot.RobotState;
 
@@ -20,8 +27,18 @@ public class ReportsView extends JPanel {
 	public ReportsView(AppServiceLocator serviceLocator, RoboServiceLocator roboServices, RobotState state) {
 		super(new GridLayout(1, 1));
 		this.roboServices = roboServices;
-		statsPanel = new SummaryReportView(serviceLocator.getMessages());
-		reportPanel = new JPanel();
+		IMessages messages = serviceLocator.getMessages();
+		UIService uis = serviceLocator.getUIService();
+		
+		statsPanel = new SummaryReportView(messages);
+		
+		S3ReportTableModel rtm = new S3ReportTableModel(messages, uis.getZoneID(), roboServices.getS3Report());
+		JTable table = new JTable(rtm);
+		table.setShowGrid(true);
+		table.setRowSorter(new TableRowSorter<>(rtm));
+		reportPanel = new JPanel(new GridLayout(1, 1));
+		reportPanel.add(new JScrollPane(table));
+		new TableModelController(rtm, uis.getFrame());
 		
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 		splitPane.setLeftComponent(statsPanel);

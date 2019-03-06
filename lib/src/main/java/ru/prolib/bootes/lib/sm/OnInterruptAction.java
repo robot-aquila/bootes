78f3f88b14f3755fadc3746ac1bcc296e5e54ec2
aquila <robot-aquila@hotmail.com>
2019-03-06 06:25:26 +0300

    lib improvements
    - timeout/finish/interrupt action delegates added

---
+++ lib/src/main/java/ru/prolib/bootes/lib/sm/OnInterruptAction.java
@@ -0,0 +1,47 @@
+package ru.prolib.bootes.lib.sm;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import ru.prolib.aquila.core.sm.SMExit;
+import ru.prolib.aquila.core.sm.SMInputAction;
+
+public class OnInterruptAction implements SMInputAction {
+	
+	public interface Handler {
+		SMExit onInterrupt(Object data);
+	}
+	
+	private final Handler handler;
+	
+	public OnInterruptAction(Handler handler) {
+		this.handler = handler;
+	}
+
+	@Override
+	public SMExit input(Object data) {
+		return handler.onInterrupt(data);
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(437809167, 61)
+				.append(handler)
+				.build();
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if ( other == this ) {
+			return true;
+		}
+		if ( other == null || other.getClass() != OnInterruptAction.class ) {
+			return false;
+		}
+		OnInterruptAction o = (OnInterruptAction) other;
+		return new EqualsBuilder()
+				.append(o.handler, handler)
+				.build();
+	}
+
+}
